127e61aa5b5a21d6dddd1c7d2f3998369ad85ba0
Olga Isakova <penumbral@yandex.ru>
2018-08-22 17:35:32 +0500

    Cleaned up app.js

--- src/app.js
+++ src/app.js
@@ -7,41 +7,8 @@ import "./styles/styles.scss";
 
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter, sortByAmount } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
 
 const store = configureStore();
-
-store.dispatch(
-  addExpense({
-    description: "Water bill",
-    amount: 300,
-    createdAt: 1000
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Gas bill",
-    amount: 200,
-    createdAt: 3000
-  })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Rent",
-    amount: 500,
-    createdAt: 2000
-  })
-);
-
-// store.dispatch(setTextFilter("water"));
-// store.dispatch(sortByAmount());
-
-const state = store.getState();
-console.log(getVisibleExpenses(state.expenses, state.filters));
 
 const app = (
   <Provider store={store}>
